061c85059056736d1ea74767238782af1ca7627c
EC2 Default User <ec2-user@ip-172-31-46-125.ec2.internal>
2021-06-15 22:20:33 +0000

    Movies

---
+++ movies/users.py
@@ -0,0 +1,52 @@
+
+import json
+import boto3
+import os
+
+users_table = os.environ['USERS_TABLE']
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(users_table)
+
+
+def getUser(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    path = event["path"]
+    user_id = path.split("/")[-1] # ["user", "id"]
+    response = table.get_item(
+        Key={
+            'pk': user_id,
+            'sk': 'age'
+        }
+    )
+    item = response['Item']
+    return {
+        'statusCode': 200,
+        'body': json.dumps(item)
+    }
+
+def putUser(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    path = event["path"]
+    user_id = path.split("/")[-1] # ["user", "id"]
+    
+    body = json.loads(event["body"])
+    print(body)
+    print(user_id)
+    item = {
+        'pk': user_id,
+        'sk': 'age',
+        'name': body["name"],
+        'last_name': body["last_name"],
+        'age': body["age"]
+    }
+    print(json.dumps(item))
+    table.put_item(
+       Item=item
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
